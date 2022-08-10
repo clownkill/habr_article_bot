2967c0cb197887c20108960448adef717699e3ac
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-08-10 19:26:59 +0300

    add saving habr article

--- habr_parser.py
+++ habr_parser.py
@@ -4,12 +4,21 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+def save_article(article_title, article_body):
+    article = f"{article_title}\n{article_body}"
+    file_name = article_title.replace(": ", " ") + ".txt"
+    print(file_name)
+
+    with open(file_name, "w", encoding="utf-8") as f:
+        f.write(article)
+
+
 def parse_habr_article(url):
     options = ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-automation'])
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_argument('--disable-blink-features=AutomationControlled')
-    options.add_argument('headless')
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("headless")
     driver = Chrome(
         service=Service(ChromeDriverManager().install()),
         options=options
@@ -18,18 +27,18 @@ def parse_habr_article(url):
 
     article_title = driver.find_element(
         By.CSS_SELECTOR,
-        'h1.tm-article-snippet__title_h1'
+        "h1.tm-article-snippet__title_h1"
     ).text
 
-    article = driver.find_element(
+    article_body = driver.find_element(
         By.CSS_SELECTOR,
-        'div.article-formatted-body'
+        "div.article-formatted-body"
     ).text
 
-    print(article)
-    print(article_title)
+    return article_title, article_body
 
 
-if __name__ == '__main__':
-    url = 'https://habr.com/ru/company/pvs-studio/blog/681824/'
-    parse_habr_article(url)
+if __name__ == "__main__":
+    url = "https://habr.com/ru/post/681798/"
+    article_title, article_body = parse_habr_article(url)
+    save_article(article_title, article_body)
